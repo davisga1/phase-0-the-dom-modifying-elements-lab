da5e15a0256dd2d073b9ff1105faa73953dd0de7
GaryDavis <davisga1999@gmail.com>
2022-07-09 03:56:43 -0500

    commit tree 9a2ccfe5d20f0d4d2a9111cbc3b745e4bc66a0b7

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("main").remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>Gary is the champion</h1>"
